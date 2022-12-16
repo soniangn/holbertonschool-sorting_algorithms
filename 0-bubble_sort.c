73baffb06b36c71d5fd544ebf4bf8db636b99d9f
soniangn <nguyen.sonia@gmail.com>
2022-12-16 06:46:46 -0800

    corrected betty errors

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,33 +1,33 @@
 #include "sort.h"
 
 /**
-* bubble_sort - entry point
-* Description: function that sorts an array 
-*            of integers in ascending order
-* @array: the array of integers
-* @size: size of the array
-* Return: 0
-*/
+ * bubble_sort - entry point
+ * Description: function that sorts an array
+ *            of integers in ascending order
+ * @array: the array of integers
+ * @size: size of the array
+ * Return: 0
+ */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i = 0;
-    size_t len = 0;
-    int tmp = 0;
+	size_t i = 0;
+	size_t len = 0;
+	int tmp = 0;
 
-    while (len < size)
-    {
-        for (; i < size - 1; i++)
-        {
-            if (array[i] > array[i + 1])
-            {
-                tmp = array[i];
-                array[i] = array[i + 1];
-                array[i + 1] = tmp;
-                print_array(array, size);
-            }
-        }
-        len++;
-        i = 0;
-    }
-    
+	while (len < size)
+	{
+		for (; i < size - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				tmp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+		len++;
+		i = 0;
+	}
+
 }
