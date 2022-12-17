4fd97dbc46ac52b2849cda54b39815d9aeabea40
soniangn <nguyen.sonia@gmail.com>
2022-12-17 09:40:45 -0800

    fixed betty errors and 1-O

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,52 +10,43 @@
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *node = NULL, *current, *azy;
-    int size = 0;
-    int len = 0;
+	int size = 0;
+	int len = 0;
 
-    azy = *list;
-    while (azy != NULL)
-    {
-        azy = azy->next;
-        size++;
-    }
-    current = *list;
-    while (len < size)
-    {
-        
-        while (current->next != NULL)
-        {
-            node = current->next;
-            
-            if (node->n < current->n)
-            {
-              /*  tmp1 = node->prev;
-                tmp2 = current->next;*/
-                
-                node->prev = current->prev;
-                current->next = node->next;
-                node->next = current;
-                
-                current->prev = node;
-                node->next->prev = node;
-                
-                current->prev->next = current;
-                if (node->prev != NULL)
-                    node->prev->next = node;
-                
-                if (current->next != NULL)
-                    current->next->prev = current;
-                 
-                if (node->prev != NULL)
-                    current = *list;
-                else
-                    *list = node;
-                print_list(*list);
+	azy = *list;
+	while (azy != NULL)
+	{
+		azy = azy->next;
+		size++;
+	}
+	current = *list;
+	while (len < size)
+	{
+		while (current->next != NULL)
+		{
+			node = current->next;
 
-            }
-            else
-                current = node;
-        }
-        len++;
-    }
+			if (node->n < current->n)
+			{
+				node->prev = current->prev;
+				current->next = node->next;
+				node->next = current;
+				current->prev = node;
+				node->next->prev = node;
+				current->prev->next = current;
+				if (node->prev != NULL)
+					node->prev->next = node;
+				if (current->next != NULL)
+					current->next->prev = current;
+				if (node->prev != NULL)
+					current = *list;
+				else
+					*list = node;
+				print_list(*list);
+			}
+			else
+				current = node;
+		}
+		len++;
+	}
 }
