42d5d552b51f11a012beedba35c88841e807c695
Marine <5641@holbertonstudents.com>
2023-01-03 10:03:08 +0100

    tache 3 : 1st push

---
+++ 3-quick_sort.c
@@ -0,0 +1,70 @@
+#include "sort.h"
+
+/**
+ * selection_sort - a function that sorts an array of int
+ *    in ascending order
+ * @array: array of int to sort
+ * @low: 0
+ * @high: size of the array
+ * @size: size of the array
+ * Return: i
+ */
+
+int partition(int *array, int low, int high, int size)
+{
+	int pivot = array[high];
+	int i = low;
+	int j, tmp;
+
+	for (j = low; j <= high; j++)
+	{
+		if (array[j] < pivot)
+		{
+			tmp = array[i];
+			array[i] = array[j];
+			array[j] = tmp;
+
+		}
+	}
+	tmp = array[i];
+	array[i] = array[high];
+	array[high] = tmp;
+
+	print_array(array, size);
+	return (i);
+}
+
+/**
+ * quicksort - a function that sorts an array of int
+ *    in ascending order
+ * @array: array of int to sort
+ * @low: 0
+ * @high: size of the array
+ * @size: size of the array
+ * Return: /
+ */
+
+void quicksort(int *array, int low, int high, int size)
+{
+	int p;
+
+	if (low < high)
+	{
+		p = partition(array, low, high, size);
+		quicksort(array, low, p - 1, size);
+		quicksort(array, p + 1, high, size);
+	}
+}
+
+/**
+ * quick_sort - a function that sorts an array of int
+ *    in ascending order
+ * @array: array of int to sort
+ * @size: size of the array
+ * Return: /
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	quicksort(array, 0, size - 1, size);
+}	
--- sort.h
+++ sort.h
@@ -27,6 +27,8 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+//void quick_sort(int *array, size_t size);
+int partition(int *array, int low, int high, int size);
+void quicksort(int *array, int low, int high, int size);
 void quick_sort(int *array, size_t size);
-
 #endif /* SORT_H */
